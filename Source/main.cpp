cb4f4350c0f023f68d1be43b6a10d089c1c9426b
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2018-08-20 03:28:54 +0100

    added basic ncurses thing

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <ncurses.h>
 
-#define WIDTH 1024
+#define WIDTH 2500
 #define HEIGHT 512
 
 int main(int argc, char* argv[])
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 	sample * myCoolSample;
 	unsigned char bitmap[WIDTH*HEIGHT*3];
 
-	if(argc > 1){
+	if(argc > 2){
 		myCoolWav = loadWav(argv[1]);
 		int x = 0;
 		for(int i = 0; x < WIDTH; i+= myCoolWav->NumberOfSamples/WIDTH ){
@@ -39,8 +39,87 @@ int main(int argc, char* argv[])
 			}
 			x++;
 		}
-
 		writeBMP(argv[2], WIDTH, HEIGHT, bitmap);
+
+		//init curses
+		initscr(); 
+		keypad(stdscr, TRUE);
+		nonl();
+		cbreak();
+		noecho();
+		bool running = true;
+		int w, h;
+		int offset = 0;
+		int c;
+		getmaxyx(stdscr, h, w);
+		int xzoom=1;
+		int szoom=1;
+		int zoom=0;
+		while(running){
+			erase();
+			int s = offset;
+			for(int x = offset; x < w+offset; x+=xzoom){
+				if (!((s < 0) || (s > myCoolWav->NumberOfSamples-1))){
+					myCoolSample = createSample(myCoolWav, s);
+					for(int y = 0; y < h; y++){
+						//mvwaddch(stdscr, y, 0, ACS_BLOCK);
+						if ( ( (((y-(h/2.0))>0) && ( (((y-(h/2.0))/(h/2.0))*32767.0) < myCoolSample->Amplitude[0]) ) ) || (((y-(h/2.0))<0) && ((((y-(h/2.0))/(h/2.0))*32768.0) > myCoolSample->Amplitude[0])) ){
+							//printw("w");
+							mvwaddch(stdscr, h-y, x-offset, ACS_BLOCK);
+						}
+						if (y-(h/2.0)==0){
+							mvwaddch(stdscr, h-y, x-offset, '-');
+						}
+					}
+					s+=szoom;
+				}
+			}
+			//printw("w: %i, h: %i\n", w, h);
+			//mvwaddch(stdscr, 0, 0, offset);
+			move(0, 0);
+			printw("char: %i, offset: %i", c, offset);
+			refresh();
+			//offset++;
+			c = getch();
+			switch(c){
+				case 27: //esc
+					running = false;
+					break;
+				case KEY_LEFT: //left
+					offset--;
+					if(offset < 0)
+						offset=0;
+					break;
+				case KEY_RIGHT: //right
+					offset++;
+					if(offset > myCoolWav->NumberOfSamples-1)
+						offset = myCoolWav->NumberOfSamples-1;
+					break;
+				case KEY_SLEFT:
+					offset-=(w/3/xzoom)*szoom/8;
+					break;
+				case KEY_SRIGHT:
+					offset+=(w/3/xzoom)*szoom/8;
+					break;
+					case 259: //zoom in
+						zoom++;
+						break;
+					case 258: //zoom out
+						zoom--;
+						break;
+				default:
+					break;
+			}
+			szoom = 1;
+			xzoom = 1;
+			if (zoom > 0){
+				xzoom=zoom;
+			}else if (zoom < 0){
+				szoom=-zoom;
+			}
+			getmaxyx(stdscr, h, w);
+		}
+		endwin();
 	}else{
 		return -1;
 	}
